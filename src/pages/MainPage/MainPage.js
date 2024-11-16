24167dfc86c315e5f7d3ae8979c6536dd00f24bf
Jinsoo-96 <tlswlstn1996@gmail.com>
2024-11-16 17:46:52 +0900

    feat: main page being prepared3

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as CalendarIcon } from "../../assets/calendar.svg";
 import { ReactComponent as AdminIcon } from "../../assets/admin_info.svg";
 import CalendarComponent from "./components/CalendarComponent";
+import MyCatsComponent from "./components/MyCatsComponent";
 import "./style/mainpage.style.css";
 
 const MainPage = () => {
@@ -46,6 +47,8 @@ const MainPage = () => {
   return (
     <div className="main-container">
       <img className="cats-room" src="backgroundimage.png" alt="cats room" />
+      <MyCatsComponent />
+
       <AdminIcon
         className="main-opt navigate-admin-button"
         onClick={() => navigate(`/admin`)}
