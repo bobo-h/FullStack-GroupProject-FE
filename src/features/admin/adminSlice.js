55948370b9c0128fec339a8ae999e3856f9cd656
Dahee Park <dahee5654@gmail.com>
2024-11-18 19:46:54 +0900

    feat : add "admin userList"

--- src/features/admin/adminSlice.js
+++ src/features/admin/adminSlice.js
@@ -2,11 +2,37 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../utils/api";
 
 // 현재 회원 불러오기 (TODO : paginate)
-export const getUserList = createAsyncThunk(
-  "admin/getUserList",
+export const getAllUserList = createAsyncThunk(
+  "admin/getAllUserList",
   async (_, { rejectWithValue }) => {
     try {
-      const response = await api.get("/admin/AllUser");
+      const response = await api.get("/admin/allUser");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+// 탈퇴 회원 but 탈퇴일로부터 90일이내  (TODO : paginate)
+export const getIneligibleUserList = createAsyncThunk(
+  "admin/getIneligibleUserList",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get("/admin/ineligibleUser");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+// 탈퇴 회원 but 탈퇴일로부터 90일 지난 회원  (TODO : paginate)
+export const getEligibleUserList = createAsyncThunk(
+  "admin/getEligibleUserList",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get("/admin/eligibleUser");
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -18,6 +44,8 @@ const adminSlice = createSlice({
   name: "admin",
   initialState: {
     allUser: [],
+    ineligibleUser: [], //탈퇴로부터 90일이내의 회원리스트
+    eligibleUser: [], //탈퇴로부터 90일이상의 회원리스트
     loading: false,
     error: "",
   },
@@ -28,14 +56,36 @@ const adminSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(getUserList.pending, (state) => {
+      .addCase(getAllUserList.pending, (state) => {
         state.loading = true;
       })
-      .addCase(getUserList.fulfilled, (state, action) => {
+      .addCase(getAllUserList.fulfilled, (state, action) => {
         state.loading = false;
-        state.allUser = action.payload.data;
+        state.allUser = action.payload.allUsers;
       })
-      .addCase(getUserList.rejected, (state, action) => {
+      .addCase(getAllUserList.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(getIneligibleUserList.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getIneligibleUserList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.ineligibleUser = action.payload.ineligibleUsers;
+      })
+      .addCase(getIneligibleUserList.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(getEligibleUserList.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getEligibleUserList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.eligibleUser = action.payload.eligibleUsers;
+      })
+      .addCase(getEligibleUserList.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
